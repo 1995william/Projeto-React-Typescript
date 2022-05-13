43103527beba215bec181e315b963cec9405c514
unknown <rwps1234@gmail.com>
2022-05-13 00:39:30 -0300

    formulario de funçao para classe

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,25 +1,29 @@
+import  {useState}  from 'react';
 import Item from './Item';
 import style from './Lista.module.scss';
 
 function Lista() {
-    const tarefas = [
-    {
-        tarefa: 'React',
-        tempo: '02:00:00'
-    }, 
-    {
-        tarefa:'Javascript',
-        tempo: '01:00:00'
-    },
-    {
-        tarefa:'Typescript',
-        tempo:'03:00:00'
-    }
-];
+    const [tarefas, setTarefas] = useState([
+        {
+            tarefa: 'React',
+            tempo: '02:00:00'
+        }, 
+        {
+            tarefa:'Javascript',
+            tempo: '01:00:00'
+        },
+        {
+            tarefa:'Typescript',
+            tempo:'03:00:00'
+        }
+    ])
+    ;
 
     return (
         <aside className={style.listaTarefas}>
-            <h2> Estudos do dia </h2>
+            <h2 onClick={() => {
+                setTarefas([...tarefas, {tarefa: 'Estudar estado', tempo: '05:00:00'}])
+            }}> Estudos do dia </h2>
             <ul>
                 { tarefas.map((item, index) => (
                    <Item 
