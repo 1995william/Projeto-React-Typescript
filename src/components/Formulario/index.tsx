e07aa41ce77423a1235baf5f1907894a95e158f8
unknown <rwps1234@gmail.com>
2022-05-16 00:04:00 -0300

    refatorando codigo

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,39 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ITarefa } from '../../types/tarefa';
 import Botao from '../Botao';
 import style from './Formulario.module.scss';
 import { v4 as uuidv4 } from 'uuid';
 
-class Formulario extends React.Component<{
-  setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
-}> {
-  state = {
-    tarefa: "",
-    tempo: "00:00"
+interface Props  {
+    setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
   }
 
-  adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+function Formulario ({setTarefas}: Props) {
+
+  const [tarefa,setTarefa] = useState('');
+  const [tempo,setTempo] = useState('00:00');
+ function adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
     evento.preventDefault();
-    this.props.setTarefas(tarefasAntigas => 
+    setTarefas(tarefasAntigas => 
       [
         ...tarefasAntigas,
         {
-          ...this.state,
+          tarefa,
+          tempo,
           selecionado: false,
           completado: false,
           id: uuidv4()
         }
       ]
-    )
-    this.setState({
-      tarefa: "",
-      tempo: "00:00"
-    })
+    );
+    setTarefa('');
+    setTempo('00:00');
   }
-
-  render() {
-    return (
-      <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
+  return (
+    <form className={style.novaTarefa} onSubmit={adicionarTarefa}>
         <div className={style.inputContainer}>
           <label htmlFor="tarefa">
             Adicione um novo estudo
@@ -42,8 +39,8 @@ class Formulario extends React.Component<{
             type="text"
             name="tarefa"
             id="tarefa"
-            value={this.state.tarefa}
-            onChange={evento => this.setState({ ...this.state, tarefa: evento.target.value })}
+            value={tarefa}
+            onChange={evento => setTarefa(evento.target.value)}
             placeholder="Digite aqui"
             required
           />
@@ -56,8 +53,8 @@ class Formulario extends React.Component<{
             type="time"
             step="1"
             name="tempo"
-            value={this.state.tempo}
-            onChange={evento => this.setState({ ...this.state, tempo: evento.target.value })}
+            value={tempo}
+            onChange={evento => setTempo(evento.target.value)}
             id="tempo"
             min="00:00:00"
             max="01:30:00"
@@ -68,8 +65,7 @@ class Formulario extends React.Component<{
           Adicionar
         </Botao>
       </form>
-    )
-  }
+  )
 }
 
 export default Formulario;
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -10,11 +10,29 @@ function App() {
   const [selecionado, setSelecionado] = useState<ITarefa>();
   
   function selecionaTarefa(tarefaSelecionada: ITarefa) {
+
     setSelecionado(tarefaSelecionada);
     setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
       ...tarefa,
       selecionado: tarefa.id === tarefaSelecionada.id ? true: false
-    })))
+    })));
+  }
+
+  function finalizarTarefa() {
+    if(selecionado) {
+      setSelecionado(undefined);
+      setTarefas(tarefasAnteriores => 
+        tarefasAnteriores.map(tarefa => {
+          if(tarefa.id === selecionado.id) {
+            return {
+              ...tarefa,
+              selecionado: false,
+              completado: true
+            }
+          }
+          return tarefa;
+        }))
+    }
   }
   return (
     <div className={style.AppStyle}>
@@ -23,7 +41,9 @@ function App() {
       tarefas={tarefas} 
       selecionaTarefa={selecionaTarefa}
       />
-      <Cronometro selecionado={selecionado} />
+      <Cronometro selecionado={selecionado} 
+      finalizarTarefa={finalizarTarefa}
+      />
     </div>
   );
 }
